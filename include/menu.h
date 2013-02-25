e7b0fb44874876dd767f6b0ad510affd7e9620e9
Paul He <paulhe55555@gmail.com>
2013-02-25 17:29:31 -0500

    Organized files, removed xenoryt

---
+++ include/menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+
+
+class Menu
+{
+    public:
+        Menu();
+        virtual ~Menu();
+    protected:
+    private:
+};
+
+#endif // MENU_H
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,24 +1,19 @@
-#include "button.h"
+#include "menu.h"
 
 /***************************************************************/
 /*******************CONSTRUCTORS********************************/
 /***************************************************************/
 
-Button::Button()
+Menu::Menu()
 {
-    w = 100;
-    h = 50;
-    x = 0;
-    y = 0;
-    bgColor = sf::Color::Black;
-    fgColor = sf::Color::White;
+    //ctor
 }
 
 /***************************************************************/
 /*******************DESTRUCTORS*********************************/
 /***************************************************************/
 
-Button::~Button()
+Menu::~Menu()
 {
     //dtor
 }
@@ -27,66 +22,10 @@ Button::~Button()
 /*******************GETTERS*************************************/
 /***************************************************************/
 
-inline int Button::getWidth()
-{
-    return w;
-}
-inline int Button::getHeight()
-{
-    return h;
-}
-inline int Button::getTop()
-{
-    return y;
-}
-inline int Button::getLeft()
-{
-    return x;
-}
-inline int Button::getRight()
-{
-    return x+w;
-}
-inline int Button::getBottom()
-{
-    return y+h;
-}
-
 /***************************************************************/
 /*******************SETTERS*************************************/
 /***************************************************************/
 
-void Button::setWidth(int i)
-{
-    w = i;
-}
-
-void Button::setHeight(int i)
-{
-    h = i;
-}
-
-void Button::setDim(int a, int b)
-{
-    w = a;
-    h = b;
-}
-
-void Button::setPos(int a, int b)
-{
-    x = a;
-    y = b;
-}
-
-void Button::setColor(sf::Color fg, sf::Color bg)
-{
-    fgColor = fg;
-    bgColor = bg;
-}
-
 /***************************************************************/
 /*******************FUNCTIONS***********************************/
 /***************************************************************/
-
-
-
