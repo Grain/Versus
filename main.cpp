697b1be9ea15896acb3a4f8d9f71e8cd8ed3a137
Paul He <paulhe55555@gmail.com>
2013-02-25 18:06:07 -0500

    Restored main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include "game.h"
+#include "global.h"
+
+Settings settings;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1024, 576), "Pro", sf::Style::Close);
+    Game game;
+
+    window.setFramerateLimit(30);
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+        window.draw(game.getSprite());
+        window.display();
+    }
+
+    return 0;
+
+}
